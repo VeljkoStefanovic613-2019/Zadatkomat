8772ba97e8cebd88d01e897250525c75ffbccd2a
VeljkoStefanovic613-2019 <veljko.s.007@gmail.com>
2023-10-26 08:23:10 +0200

    first commit

---
+++ src/components/AddTask.js
@@ -0,0 +1,37 @@
+
+export const AddTask = ({tasklist, setTasklist, task, setTask}) => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(task.id){
+            const date = new Date();
+            const updatedTaskList = tasklist.map((todo) => (
+                todo.id === task.id ? {id: task.id, name: task.name, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo
+            ));
+            setTasklist(updatedTaskList);
+            setTask({});
+        }
+        else{
+            const date = new Date();
+            const newTask = {
+                id: date.getTime() , 
+                name: e.target.task.value, 
+                time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+            }  
+           setTasklist([...tasklist, newTask]);
+           setTask({});  
+        }
+
+    }
+
+  return (
+    <section className="addTask">
+        <form onSubmit={handleSubmit}>
+            <input type="text"  name="task" value={task.name || ""} autoComplete="off" placeholder="Dodaj Zadatak " maxLength="25" onChange={e => setTask({...task, name: e.target.value})}/>
+            <button type="submit">{ task.id ? "Azuriraj" : "Dodaj" }</button>
+        </form>
+
+    </section>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import Logo from "../assets/logo.png"
+
+export const Header = () => {
+    
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+        document.documentElement.removeAttribute("class");
+        document.documentElement.classList.add(theme);
+
+    }, [theme]);
+
+    
+
+
+  return (
+   <header>
+        <div className="logo">
+            <img src={Logo} alt="Zadatkomat logo" />
+            <span>Zadatkomat</span>
+        </div>
+        <div className="themeSelector">
+            <span onClick={() => setTheme("light")} className={theme === "light" ? "light activeTheme" : "light"}></span>
+            <span onClick={() => setTheme("medium")} className={theme === "medium" ? "medium activeTheme" : "medium"}></span>
+            <span onClick={() => setTheme("dark")} className={theme === "dark" ? "dark activeTheme" : "dark"}></span>
+            <span onClick={() => setTheme("gOne")} className={theme === "gOne" ? "gOne activeTheme" : "gOne"}></span>
+            <span onClick={() => setTheme("gTwo")} className={theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}></span>
+            <span onClick={() => setTheme("gThree")} className={theme === "gThree" ? "gThree activeTheme" : "gThree"}></span>
+            
+        </div>
+   </header>
+  )
+}
